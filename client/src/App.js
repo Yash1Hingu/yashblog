d219454d856cd4b4fe3331f9cfc1bc6569da2b8a
Yash1Hingu <yash23hingu@gmail.com>
2023-12-24 12:07:39 +0530

    Added Navbar with style

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <header>
+        <a href="" className="logo">Yash-Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
   );
 }
 
