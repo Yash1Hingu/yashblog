056038497fbe14ed2280db235a74343fbd50cc3d
Yash1Hingu <yash23hingu@gmail.com>
2023-12-24 14:59:02 +0530

    Added Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Header from './components/Header';
-import Post from './components/Post';
 import './App.css';
 import Layout from './components/Layout';
 import IndexPage from './components/pages/IndexPage';
+import LoginPage from './components/pages/LoginPage';
 
 function App() {
   return <Routes>
     <Route path='/' element={<Layout />}>
-      <Route index element={<IndexPage>} />
-      <Route path='/login' element={<h1>Login</h1>} />
+      <Route index element={<IndexPage/>} />
+      <Route path='/login' element={<LoginPage/>} />
       <Route path='/register' element={<h1>Register</h1>} />
     </Route >
   </Routes>
