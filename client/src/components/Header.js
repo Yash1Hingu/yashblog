c4717a3efb132ec93c6e2ff13997cbf238b0bbee
Yash1Hingu <yash23hingu@gmail.com>
2023-12-24 13:35:45 +0530

    Seperate Components

---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return <header>
+        <a href="" className="logo">Yash-Blog</a>
+        <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+        </nav>
+    </header>
+}
---
+++ client/src/components/Post.js
@@ -0,0 +1,15 @@
+export default function Post() {
+    return <div className="post">
+        <div className="image">
+            <img src="https://images.unsplash.com/photo-1517329782449-810562a4ec2f?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" />
+        </div>
+        <div className="texts">
+            <h2>New to the web platform in December</h2>
+            <p className="info">
+                <a href="" className="author">yash1hingu</a>
+                <time>2023-01-06 16:34</time>
+            </p>
+            <p className='summary'>Discover some of the interesting features that have landed in stable and beta web browsers during December 2023.</p>
+        </div>
+    </div>
+}
