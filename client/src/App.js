8931fc5239343f2accd823273cea7100c329ddb6
Yash1Hingu <yash23hingu@gmail.com>
2023-12-24 12:24:54 +0530

    Added Posts with style

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,33 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1517329782449-810562a4ec2f?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" />
+        </div>
+        <div className="texts">
+          <h2>New to the web platform in December</h2>
+          <p>Discover some of the interesting features that have landed in stable and beta web browsers during December 2023.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1517329782449-810562a4ec2f?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" />
+        </div>
+        <div className="texts">
+          <h2>New to the web platform in December</h2>
+          <p>Discover some of the interesting features that have landed in stable and beta web browsers during December 2023.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1517329782449-810562a4ec2f?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" />
+        </div>
+        <div className="texts">
+          <h2>New to the web platform in December</h2>
+          <p>Discover some of the interesting features that have landed in stable and beta web browsers during December 2023.</p>
+        </div>
+      </div>
     </main>
   );
 }
