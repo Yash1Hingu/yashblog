ba656f420263467a324c46d6442e4efb01b32ab1
Yash1Hingu <yash23hingu@gmail.com>
2023-12-24 23:03:58 +0530

    Added bycrypt

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const UserModel = require('./models/User');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +17,10 @@ app.post("/register", async (req, res) => {
     const { userName, userPassword } = req.body;
 
     try {
-        const userDoc = await UserModel.create({ userName, userPassword });
+        const userDoc = await UserModel.create({
+            userName,
+            userPassword: bcrypt.hashSync(userPassword, salt)
+        });
         res.json(userDoc);
     } catch (e) {
         res.json(e);
@@ -23,7 +29,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { userName, userPassword } = req.body;
-    const userDoc = await UserModel.findOne({userName});
+    const userDoc = await UserModel.findOne({ userName });
     res.json(userDoc);
 })
 app.listen(4000, () => {
