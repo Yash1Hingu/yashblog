7d10faa8a227c7fed2117b21e2cdf0af483145e5
Yash1Hingu <yash23hingu@gmail.com>
2023-12-25 16:58:56 +0530

    Redirected when user created post

--- client/src/components/pages/CreatePost.js
+++ client/src/components/pages/CreatePost.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { Navigate } from 'react-router-dom';
 
 const modules = {
     toolbar: [
@@ -23,18 +24,27 @@ export default function CreatePost() {
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
     const [files, setFiles] = useState('');
-    function createNewPost(ev) {
+    const [redirect, setRedirect] = useState(false);
+    async function createNewPost(ev) {
         const data = new FormData();
         data.set('title', title);
         data.set('summary', summary);
         data.set('content', content);
-        data.set('file',files[0]);
+        data.set('file', files[0]);
         ev.preventDefault();
         console.log(files);
-        fetch('http://localhost:4000/post', {
+        const response = await fetch('http://localhost:4000/post', {
             method: 'POST',
             body: data,
         })
+
+        if (response.ok) {
+            setRedirect(true);
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={'/'} />
     }
     return <form onSubmit={createNewPost}>
         <input
