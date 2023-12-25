bdbfd4a1cdbbff02aac9f24b914df479c677ae45
Yash1Hingu <yash23hingu@gmail.com>
2023-12-25 15:43:37 +0530

    Added create post

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import IndexPage from './components/pages/IndexPage';
 import LoginPage from './components/pages/LoginPage';
 import RegisterPage from './components/pages/RegisterPage';
 import { UserContextProvider } from './store/user-context';
+import CreatePost from './components/pages/CreatePost';
 
 function App() {
   return <UserContextProvider>
@@ -14,6 +15,7 @@ function App() {
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/create' element={<CreatePost />} />
       </Route >
     </Routes>
   </UserContextProvider>
