679c71cbca0c4518805dd081b54a9dac9b056b1c
Yash1Hingu <yash23hingu@gmail.com>
2023-12-25 16:02:23 +0530

    Added input field in createpost with state

--- client/src/components/pages/CreatePost.js
+++ client/src/components/pages/CreatePost.js
@@ -1,3 +1,50 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+    ],
+};
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
+
 export default function CreatePost() {
-    return <h1>create post</h1>
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+
+    return <form>
+        <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(ev) => { setTitle(ev.target.value) }}
+        />
+        <input
+            type="summary"
+            placeholder="Summary"
+            value={summary}
+            onChange={(ev) => { setSummary(ev.target.value) }}
+        />
+        <input
+            type="file"
+        />
+        <ReactQuill
+            value={content}
+            onChange={newValue => { setContent(newValue) }}
+            modules={modules}
+            formats={formats}
+        />
+        <button>Create Post</button>
+    </form>
 }
