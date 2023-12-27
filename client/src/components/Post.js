92849db12f43e05d51071ff68685c29df00dafd5
Yash1Hingu <yash23hingu@gmail.com>
2023-12-27 09:49:17 +0530

    Added Link and CSS

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,15 +1,20 @@
 import { format } from 'date-fns';
-export default function Post({ title, summary, content, createdAt, cover ,author}) {
-    const shortSummary = summary.length > 100 ? summary.slice(0,100)+"...": summary;
+import { Link } from 'react-router-dom';
+export default function Post({ title, summary, content, createdAt, cover, author }) {
+    const shortSummary = summary.length > 100 ? summary.slice(0, 100) + "..." : summary;
     return <div className="post">
         <div className="image">
-            <img src={'http://localhost:4000/'+cover} alt="" />
+            <Link to={'/post/id'}>
+                <img src={'http://localhost:4000/' + cover} alt="" />
+            </Link>
         </div>
         <div className="texts">
-            <h2>{title}</h2>
+            <Link to={'/post/id'}>
+                <h2>{title}</h2>
+            </Link>
             <p className="info">
                 <a href="" className="author">{author.userName}</a>
-                <time>{format(new Date(createdAt),'MMM d, yyyy HH:mm')}</time>
+                <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
             </p>
             <p className='summary'>{shortSummary}</p>
         </div>
