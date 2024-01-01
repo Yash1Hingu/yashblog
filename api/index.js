f8f352f4b3b7908fbb95f4e068828e4a2de1a260
Yash1Hingu <yash23hingu@gmail.com>
2024-01-01 22:05:08 +0530

    Added cors

--- api/index.js
+++ api/index.js
@@ -15,13 +15,18 @@ const app = express();
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRETJWT;
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({
+    origin: "https://yashblog.vercel.app",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"],
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 mongoose.connect(process.env.MONGODB_ATLAS);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     console.log('hello');
 })
 
@@ -118,7 +123,7 @@ app.put('/post', uploadMiddlewear.single('file'), async (req, res) => {
         const { title, summary, content, id } = req.body;
         const postDoc = await PostModel.findById(id);
         const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-        if(!isAuthor) {
+        if (!isAuthor) {
             return res.status(400).json('you are not a Author');
         }
         await postDoc.updateOne({
