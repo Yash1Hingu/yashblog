9123bd7b30345e127040a64f5cb7f79824e83cc5
Yash1Hingu <yash23hingu@gmail.com>
2024-01-03 15:26:26 +0530

    Added CORS

--- client/src/components/pages/IndexPage.js
+++ client/src/components/pages/IndexPage.js
@@ -4,18 +4,7 @@ import Post from "../Post";
 export default function IndexPage() {
     const [posts, setPosts] = useState([]);
     useEffect(() => {
-        fetch('https://yashblogapi.onrender.com/post',
-            {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Credentials": true,
-                    "Access-Control-Allow-Origin": "https://yashblogapi.onrender.com"
-                }
-            }
-        ).then(response => {
+        fetch('https://yashblogapi.onrender.com/post').then(response => {
             response.json().then(posts => {
                 setPosts(posts);
             })
