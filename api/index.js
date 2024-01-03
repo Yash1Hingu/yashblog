8e54f7f25ef12c6f467c00eb7fb7c59be1827ddd
Yash1Hingu <yash23hingu@gmail.com>
2024-01-03 15:47:35 +0530

    Added manually cors

--- api/index.js
+++ api/index.js
@@ -20,8 +20,28 @@ app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 mongoose.connect(process.env.MONGODB_ATLAS);
 app.use(cors({ credentials: true, origin: /\.onrender\.com$/ }));
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://yashblogs.onrender.com"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
 
-app.get('/',(req,res)=>{
+    next();
+});
+
+app.get('/', (req, res) => {
     console.log('hello');
 })
 
@@ -118,7 +138,7 @@ app.put('/post', uploadMiddlewear.single('file'), async (req, res) => {
         const { title, summary, content, id } = req.body;
         const postDoc = await PostModel.findById(id);
         const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-        if(!isAuthor) {
+        if (!isAuthor) {
             return res.status(400).json('you are not a Author');
         }
         await postDoc.updateOne({
